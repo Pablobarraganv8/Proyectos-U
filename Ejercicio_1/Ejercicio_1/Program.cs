73f2a280d3d7672ca5c88763b6d0e8d29aa53d13
Pablobarraganv8 <167924522+Pablobarraganv8@users.noreply.github.com>
2024-04-23 17:48:45 -0500

    Add files via upload
    EJERCICIO 1

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {#include <iostream>
+
+        static void Main(string[] args)
+        {
+            int main()
+            {
+                // Declaración de variables
+                double capitalInicial, capitalActual, interesMensual, interes;
+                int numeroMeses;
+
+                // Pedir al usuario que ingrese el capital inicial y el número de meses
+                cout << "Ingrese el capital inicial: ";
+                cin >> capitalInicial;
+                cout << "Ingrese el número de meses: ";
+                cin >> numeroMeses;
+
+                // Calcular el interés mensual
+                interesMensual = 0.7 / 100;
+
+                // Inicializar el capital actual con el capital inicial
+                capitalActual = capitalInicial;
+
+                // Mostrar el encabezado
+                cout << "Mes | Capital" << endl;
+
+                // Iterar sobre cada mes
+                for (int mes = 1; mes <= numeroMeses; mes++)
+                {
+                    // Calcular el interés del mes actual
+                    interes = capitalActual * interesMensual;
+
+                    // Añadir el interés al capital actual
+                    capitalActual = capitalActual + interes;
+
+                    // Mostrar el capital actual para este mes
+                    cout << mes << " | " << capitalActual << endl;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
