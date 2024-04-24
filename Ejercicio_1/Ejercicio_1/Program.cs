140acb3ed4551990ff6063d56ec41896aa663427
Paulo Andrés Camacho Solimán <85325313+KelevsID@users.noreply.github.com>
2024-04-23 19:16:07 -0500

    Update Program.cs

--- Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -1,52 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Ejercicio_1
+namespace testCS
 {
-    internal class Program
-    {#include <iostream>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double capitalInicial, capitalActual, interes, interesMensual, numeroMeses;
 
-        static void Main(string[] args)
-        {
-            int main()
-            {
-                // Declaración de variables
-                double capitalInicial, capitalActual, interesMensual, interes;
-                int numeroMeses;
+			// Pedir al usuario que ingrese el capital inicial y el número de meses
+			Console.WriteLine("Ingrese el capital inicial: ");
+			capitalInicial = double.Parse(Console.ReadLine());
+			// Console.WriteLine(capitalInicial);
 
-                // Pedir al usuario que ingrese el capital inicial y el número de meses
-                cout << "Ingrese el capital inicial: ";
-                cin >> capitalInicial;
-                cout << "Ingrese el número de meses: ";
-                cin >> numeroMeses;
+			Console.WriteLine("Ingrese el número de meses: ");
+			numeroMeses = double.Parse(Console.ReadLine());
+			// Console.WriteLine(numeroMeses);
 
-                // Calcular el interés mensual
-                interesMensual = 0.7 / 100;
+			// Calcular el interés mensual
+			interesMensual = 0.7 / 100;
+			// Inicializar el capital actual con el capital inicial
+			capitalActual = capitalInicial;
 
-                // Inicializar el capital actual con el capital inicial
-                capitalActual = capitalInicial;
+			// Mostrar el encabezado
+			Console.WriteLine("Mes | Capital");
+			for (int mes = 1; mes <= numeroMeses; mes++)
+			{
+				// Calcular el interés del mes actual
+				interes = capitalActual * interesMensual;
 
-                // Mostrar el encabezado
-                cout << "Mes | Capital" << endl;
+				// Añadir el interés al capital actual
+				capitalActual = capitalActual + interes;
 
-                // Iterar sobre cada mes
-                for (int mes = 1; mes <= numeroMeses; mes++)
-                {
-                    // Calcular el interés del mes actual
-                    interes = capitalActual * interesMensual;
-
-                    // Añadir el interés al capital actual
-                    capitalActual = capitalActual + interes;
-
-                    // Mostrar el capital actual para este mes
-                    cout << mes << " | " << capitalActual << endl;
-                }
-
-                return 0;
-            }
-        }
-    }
+				// Mostrar el capital actual para este mes
+				Console.WriteLine(mes + " | "  + capitalActual);
+			}
+		}
+	}
 }
